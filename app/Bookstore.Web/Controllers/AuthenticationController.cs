cec98bdc20edab1513811b42f537062c3c0f246a
NoReply Amazon <no-reply@amazon.com>
2025-02-06 11:21:33 +0000

    [Commit] Iteration 21 #errors = 3 <= 8.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Controllers/AuthenticationController.cs][CS0246][40,42] The type or namespace name 'HttpCookie' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Controllers/AuthenticationController.cs
+++ app/Bookstore.Web/Controllers/AuthenticationController.cs
@@ -35,14 +35,14 @@ namespace Bookstore.Web.Controllers
 
         private ActionResult CognitoSignOut()
         {
-            if (Request.Cookies[".AspNet.Cookies"] != null)
+            if (Request.Cookies.ContainsKey(".AspNet.Cookies"))
             {
-                Response.Cookies.Add(new HttpCookie(".AspNet.Cookies") { Expires = DateTime.Now.AddDays(-1) });
+                Response.Cookies.Delete(".AspNet.Cookies");
             }
 
             var domain = BookstoreConfiguration.Get("Authentication/Cognito/CognitoDomain");
             var clientId = BookstoreConfiguration.Get("Authentication/Cognito/LocalClientId");
-            var logoutUri = $"{Request.Url.Scheme}://{Request.Url.Host}:{Request.Url.Port}/";
+            var logoutUri = $"{Request.Scheme}://{Request.Host}/";
 
             return Redirect($"{domain}/logout?client_id={clientId}&logout_uri={logoutUri}");
         }
